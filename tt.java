29bc3f25367e089f24fcce81434d1a8522cd43e7
Manish Thakur <36801747+Manishthakur1297@users.noreply.github.com>
2019-02-21 17:07:54 +0530

    Update tt.java

--- tt.java
+++ tt.java
@@ -6,22 +6,24 @@ public class MainMenu
         int s_choice = 1;
         do
         {
-            System.out.println("1. Clerk");
+            System.out.println("\n1. Clerk");
 		    System.out.println("2. Programmer");
-		    System.out.println("3. manager");
+		    System.out.println("3. Manager");
 		    System.out.println("4. Exit");
 		    System.out.print("Enter choice : ");
 		    s_choice = sc.nextInt();
 		    sc.nextLine();
 		    switch(s_choice)
 		    {
+		        //count++;
 		        case 1:
+		            c_arr.add(new Clerk());
 		            break;
 		        case 2:
-		            //Display
+		            c_arr.add(new Programmer());
 		            break;
 		        case 3:
-		            //Raise
+		            c_arr.add(new Manager());
 		            break;
 		        case 4:
 		            //Exit
@@ -33,13 +35,13 @@ public class MainMenu
     
 	public static void main(String[] args) {
 		int m_choice = 1;
-		public static count = 0;
-		public static ArrayList<Clerk> c_arr = new ArrayList<Clerk>();
-		public static ArrayList<Programmer> p_arr = new ArrayList<Programmer>();
-		public static ArrayList<Manager> m_arr = new ArrayList<Manager>();
+		//public static count = 0;
+		public static ArrayList<Emp> c_arr = new ArrayList<Emp>();
+		//public static ArrayList<Programmer> p_arr = new ArrayList<Programmer>();
+		//public static ArrayList<Manager> m_arr = new ArrayList<Manager>();
 		do
 		{
-		    System.out.println("1. Create");
+		    System.out.println("\n1. Create");
 		    System.out.println("2. Display");
 		    System.out.println("3. Raise Salary");
 		    System.out.println("4. Exit");
@@ -49,9 +51,11 @@ public class MainMenu
 		    switch(m_choice)
 		    {
 		        case 1:
+		            MainMenu.subMenu();
 		            break;
 		        case 2:
 		            //Display
+		            
 		            break;
 		        case 3:
 		            //Raise
